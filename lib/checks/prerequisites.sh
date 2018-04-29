5bb92168cf866901b6ccc801cd7d5be26191baa3
axano <okmanolos1@hotmail.com>
2018-04-29 18:44:09 -0400

    autoCommit

--- lib/checks/prerequisites.sh
+++ lib/checks/prerequisites.sh
@@ -8,14 +8,15 @@ preReqs=(
 	 'moo'
 
 )
+echo ''
 for i in "${preReqs[@]}"
 do :
 
    condition=$(which $i 2>/dev/null | grep -v "not found" | wc -l)
    if [ $condition -eq 0 ] ; then
-    echo -e "\e[0;31m$i is not installed\e[m"
+     echo -e "\e[0;31m[-]$i is not installed\e[m"
    else
-   echo "$i is installed"
+     echo "[+]$i is installed"
   fi
 done
 }
