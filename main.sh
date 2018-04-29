b11a65482aba0b9019ae3770e19d2a9106225b91
axano <okmanolos1@hotmail.com>
2018-04-29 18:09:36 -0400

    test

--- main.sh
+++ main.sh
@@ -9,7 +9,7 @@ _   _
 |  _  |/ _ \ '__| '_ ` _ \ / _ \/ __|
 | | | |  __/ |  | | | | | |  __/\__ \
 \_| |_/\___|_|  |_| |_| |_|\___||___/
-web pentest framework created by AXANO
+pentest framework created by AXANO
 EOF
 }
 
