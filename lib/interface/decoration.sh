18501d86bd3b13c4b724f6ae2f4c829ece461c66
axano <okmanolos1@hotmail.com>
2018-04-29 18:19:42 -0400

    autoCommit

---
+++ lib/interface/decoration.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#This colour
+cyan='\e[0;36m'
+green='\e[0;32m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+resetCollor='\e[m'
+
+
+fancyEcho()
+{
+collors=(
+	'\e[0;36m'
+	'\e[0;32m'
+	'\e[1;32m'
+	'\e[1;31m'
+	'\e[1;37m'
+	'\e[1;33m'
+	'\e[1;34m'
+	'\e[1;35m'
+)
+string=$1
+for word in $string;
+do
+#arr=(`echo ${string}`);
+arr=( $(echo ${word} | sed "s/\(.\)/\1 /g") )
+for i in "${arr[@]}"
+    do
+	collor=${collors[$RANDOM % ${#collors[@]} ]}
+	echo -ne $collor$i$resetCollor
+	sleep 0.06
+done
+echo -n " "
+done
+#echo -ne "\n"
+unset IFS
+}
+
+banner()
+{
+clear
+cat << "EOF"
+
+
+EOF
+
+fancyEcho "Web Pentesting Framework brought to you AXANO"
+
+}
