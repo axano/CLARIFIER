ee644542c02f650a1be96f5e69741f8f4fc3fba8
axano <okmanolos1@hotmail.com>
2018-04-29 18:39:55 -0400

    autoCommit

---
+++ lib/checks/prerequisites.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+checkPrerequisites(){
+#array with prerequisites
+preReqs=(
+   'nc'
+   'apache2'
+	 'moo'
+
+)
+for i in "${preReqs[@]}"
+do :
+
+   condition=$(which $i 2>/dev/null | grep -v "not found" | wc -l)
+   if [ $condition -eq 0 ] ; then
+    echo -e "\e[0;31m$i is not installed\e[m"
+   else
+   echo "$i is installed"
+  fi
+done
+}
