de4b7060f05c1ef2446c637f1fa40f83248582b8
axano <okmanolos1@hotmail.com>
2018-05-17 16:30:05 -0400

    Commited with script.

--- lib/phases/intelligentFootprinting.sh
+++ lib/phases/intelligentFootprinting.sh
@@ -4,7 +4,7 @@
 #Follows redirects but default port is 80
 #Is silenced by adding "> /dev/null" on the end
 startNikto(){
-echoLog "Starting nikto test. May take a while [avg. 5 min]..."
+echoLog "Starting nikto test. May take a while [avg. 10 min]..."
 while IFS= read -r line; do
   nikto -h $line -o $path/reports/$1/$line/niktoResults.txt > /dev/null
 done < "$path/reports/$1/subdomains.txt"
